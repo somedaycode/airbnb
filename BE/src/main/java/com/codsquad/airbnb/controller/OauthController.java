4b0d81bbcb8aee82fc7d402d38413215b1e9444e
pbg0205 <pbg0205@naver.com>
2021-06-02 01:33:37 +0900

    style : OauthController 불필요 import 제거

--- BE/src/main/java/com/codsquad/airbnb/controller/OauthController.java
+++ BE/src/main/java/com/codsquad/airbnb/controller/OauthController.java
@@ -1,25 +1,15 @@
 package com.codsquad.airbnb.controller;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.codsquad.airbnb.dto.Code;
-import com.codsquad.airbnb.dto.GithubAccessTokenRequestDto;
 import com.codsquad.airbnb.dto.GithubAccessTokenResponseDto;
 import com.codsquad.airbnb.dto.User;
 import com.codsquad.airbnb.service.OauthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
