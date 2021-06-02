2044144813aee5fb606f40010e1e4a5cefbb0528
pbg0205 <pbg0205@naver.com>
2021-06-02 15:55:11 +0900

    feat : User class getter, setter lombok으로 변경

--- BE/src/main/java/com/codsquad/airbnb/dto/User.java
+++ BE/src/main/java/com/codsquad/airbnb/dto/User.java
@@ -1,34 +1,19 @@
 package com.codsquad.airbnb.dto;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class User {
     private String login;
     private String name;
 
-    public User() {
-    }
-
     public User(String login, String name) {
         this.login = login;
-        this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @JsonSetter("login")
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    @JsonSetter("name")
-    public void setName(String name) {
         this.name = name;
     }
 }
