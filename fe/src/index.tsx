f54cd31fc6587024199ae40afd05fd6e7ece90bd
somedaycode <somedaycode@gmail.com>
2021-08-27 00:29:42 +0900

    Refactor: index.tsx import 경로 코드 라인 정리

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { RecoilRoot } from 'recoil';
 
+import App from './App';
 import '@styles/reset.css';
 
 ReactDOM.render(
